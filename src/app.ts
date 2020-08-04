8554c6d55265ccea4fbcd0068b1dc2b2832a600a
Collin Howland <collin.howland@hotmail.com>
2020-08-04 14:52:20 -0700

    added environment config stuff

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
-import express = require('express');
+import express from 'express'
+import bodyParser from 'body-parser'
 
 // Create a new express application instance
-const app: express.Application = express();
+// const app: express.Application = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.use(bodyParser.json())
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 8000!');
-});
+// app.get('/hello', (req, res) => {
+//   res.send('Hello!');
+// });
+
+// app.get('/hello/:name', (req, res) => {
+//   res.send(`Hello ${req.params.name}`)
+// })
+
+// app.post('/hello', (req, res) => {
+//   res.send(`Hello ${req.body.name}`)
+// })
+
+// const server = app.listen(8000, () => {
+//   console.log('Example app listening on port 8000!');
+// });
+
